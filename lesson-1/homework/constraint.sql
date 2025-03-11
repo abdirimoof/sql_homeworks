5c3d33911647d43b4859b50ce5333c6add1d94b2
Jaxon <101799444+abdirimoof@users.noreply.github.com>
2025-03-11 22:29:53 +0500

    lesson1.sql

--- lesson-1/homework/constraint.sql
+++ lesson-1/homework/constraint.sql
@@ -1,1 +1,30 @@
-select 1
+-- homework first day
+
+create database homeworkDB
+use homeworkDB
+
+create table Students (
+StudentId int,
+FullName varchar(50),
+Age int,
+GPA decimal(3,2)
+)
+
+Select * from Students
+
+Alter table Students
+add Email varchar(50)
+
+sp_rename 'students.Fullname', 'Name', 'column' 
+
+Alter table Students
+drop column Age
+
+truncate table Students
+
+select @@SERVERNAME AS [SERVER NAME]
+
+select GETDATE() as [current date and time]
+
+select * from Students
+
